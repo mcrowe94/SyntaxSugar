35b67f0b0414f87968d134dc74814d9d97514495
Monica Crowe <mcrowe9194@gmail.com>
2023-04-13 10:15:43 -0500

    done

---
+++ Program.cs
@@ -0,0 +1,14 @@
+﻿namespace SyntaxSugar
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            var answer = 4;
+
+            var response = (answer < 9) ? $"{answer} is less than nine" : $"{answer} is greater than or equal to nine";
+
+            Console.WriteLine(response);
+        }
+    }
+}
